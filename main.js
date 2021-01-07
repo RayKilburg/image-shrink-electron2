7a1f58c0412048f87d5fbf51554025e9eac8f256
RayKilburg <rkilburg42@gmail.com>
2021-01-07 01:56:18 -0500

    IPC Communication

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu } = require('electron')
+const { app, BrowserWindow, Menu, ipcMain } = require('electron')
 
 // Set env
 process.env.NODE_ENV = 'developement'
@@ -89,6 +89,10 @@ const menu = [
         }
     ] : [])
 ]
+
+ipcMain.on('image:minimize', (e, options) => {
+    console.log(options)
+})
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
